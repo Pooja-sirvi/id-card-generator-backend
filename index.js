69a3433a41d5a5524edfa6fbaf3526d080a540fc
Pooja-sirvi <sirvipooja02@gmail.com>
2024-12-02 01:24:51 +0530

    changes

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.get('/', (req, res) => {
   res.send('ID Card App');
 });
 
+app.get("/op", (req, res) => {
+  res.send("ID Card App");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
